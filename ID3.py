b3aa027141097191e3d08214eb564032e0a58aa0
bikashSingh-dec <112297449+bikashSingh-dec@users.noreply.github.com>
2022-08-28 10:39:45 +0530

    Update and rename main.py to ID3.py

--- ID3.py
+++ ID3.py
@@ -24,11 +24,3 @@ import matplotlib.pyplot as plt
 fig=plt.figure(figsize=(16,12))
 a=plot_tree(dtree, feature_names=df_getdummy.columns, fontsize=12, filled=True, class_names=['Not play', 'play'])
 
-#Gini index
-from sklearn import tree
-clf=tree.DecisionTreeClassifier(criterion='gini')
-clf=clf.fit(x_train,y_train)
-import graphviz
-dot_data=tree.export_graphviz(clf, out_file=None)
-graph=graphviz.Source(dot_data)
-graph
